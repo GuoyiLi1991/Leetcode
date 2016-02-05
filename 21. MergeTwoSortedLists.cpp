a986469b82131fe81887ea277daae68ac4256fd5
wxn7 <Ning@campus-007-077.ucdavis.edu>
2016-02-04 17:57:10 -0800

    update
    Faster. No need to create new nodes every time!

--- 21. MergeTwoSortedLists.cpp
+++ 21. MergeTwoSortedLists.cpp
@@ -9,41 +9,27 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        ListNode * l3 = NULL; //store result
-        if (!l1 && !l2) return l3;
-        if (!l1) return l2;
-        if (!l2) return l1;
+        if (!l1 || !l2) return (l1)? l1:l2;
         
-       // ListNode* p1 = l1, *p2 = l2;
-        ListNode* tail = NULL;
-        ListNode *tmp;
+        ListNode * l3 = new ListNode(0); //store result
+        ListNode* tail = l3;
         while(l1 && l2)
         {
             if (l1->val <= l2->val)
             {
-                tmp  = new ListNode(l1->val);
+                tail->next = l1;
                 l1 = l1->next;
             }
             else
             {
-                tmp  = new ListNode(l2->val);
+                tail->next = l2;
                 l2 = l2->next;
             }
-            //insert tmp to l3 head
-            if (!l3) 
-            {
-                l3 = tmp;
-                tail = tmp;
-            }
-            else
-            {
-                tail->next = tmp;
-                tail = tail->next;
-            }
+            tail = tail->next;
         }
         if (l1) tail->next = l1;
         else if (l2) tail->next = l2;
         
-        return l3;
+        return l3->next;
     }
 };
