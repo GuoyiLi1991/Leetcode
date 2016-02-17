a27aa02e1a2a47fd9139af0681b9e8b58a2100c4
wxn7 <Ning@Nings-Macbook.local>
2016-02-16 20:25:12 -0800

    2 solutions

--- 268.MissingNumber.cpp
+++ 268.MissingNumber.cpp
@@ -3,11 +3,20 @@ public:
     int missingNumber(vector<int>& nums) {
         int n = nums.size();
         int result = 0;
+        
+        //solution 1: sum them up
         for (int i = 0; i<n; i++)
         {
-            result = result + (i-nums[i]);
+            result += i-nums[i];
         }
         return result+n;
+
+        //solution 2: Bit maniputaion
+        // int ans = 0;
+        // for (int i = 0; i<n; i++)
+        //     ans ^= nums[i] ^ i;
+        // return ans^n;
         
     }
 };
+        
