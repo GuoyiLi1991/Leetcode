c0fe0d43ae36e29ec12debdae1261e94b57e3a4e
flyvbee <wxn.vicky@gmail.com>
2016-07-07 21:56:39 -0700

    switch way

--- 20. ValidParenthesis.cpp
+++ 20. ValidParenthesis.cpp
@@ -20,3 +20,33 @@ public:
         return st.empty();
     }
 };
+
+
+
+/////////////////////////////////////
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for (char c:s) {
+            switch (c) {
+                case '[':
+                case '{':
+                case '(': st.push(c); break;
+                case ']': 
+                    if (st.empty() || st.top() != '[' ) 
+                        return false;
+                    st.pop();break;
+                case '}':
+                    if (st.empty() || st.top() != '{' ) 
+                        return false;
+                    st.pop();break;
+                case ')':
+                    if (st.empty() || st.top() != '(') 
+                        return false;
+                    st.pop();break;
+            }
+        }
+        return st.empty();
+    }
+};
