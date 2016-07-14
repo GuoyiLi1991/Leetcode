1faf3b8d4139021f53d4a590fff59a4e2edb331d
flyvbee <wxn.vicky@gmail.com>
2016-07-13 18:30:50 -0700

    my version of merging interval

--- 56. MergeIntervals.cpp
+++ 56. MergeIntervals.cpp
@@ -26,4 +26,55 @@ public:
         return ans;
         
     }
+
+
+    //////////// My version ////////////////
+    /**
+ * Definition for an interval.
+ * struct Interval {
+ *     int start;
+ *     int end;
+ *     Interval() : start(0), end(0) {}
+ *     Interval(int s, int e) : start(s), end(e) {}
+ * };
+ */
+class Solution {
+public:
+    vector<Interval> merge(vector<Interval>& intervals) {
+        int n = intervals.size();
+        vector<Interval> res;
+        
+        if (n < 2) return intervals;
+        
+        sort(intervals.begin(), intervals.end(), 
+                [](Interval i1, Interval i2)
+                {
+                    return i1.start<i2.start;
+                });
+                
+        int start = intervals[0].start;
+        int end = intervals[0].end;
+        
+    
+        for (int i = 1; i < n; i++) {
+            Interval cur = intervals[i];
+            
+            if (cur.start > end) //no merging
+            {
+                Interval tmp(start, end);
+                res.push_back(tmp);
+                start = cur.start;
+                end = cur.end;
+            }
+            
+            if (cur.end > end) {
+                end = cur.end;
+            }
+        }
+        
+        Interval tmp(start,end);
+        res.push_back(tmp);
+        return res;
+    }
+};
 };
