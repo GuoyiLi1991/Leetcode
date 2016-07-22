52d4c52a5596a2a59688d0046ada1456e451c52d
flyvbee <wxn.vicky@gmail.com>
2016-07-21 22:21:16 -0700

    update sorted array to BST

--- 108. SortedArray2BST.cpp
+++ 108. SortedArray2BST.cpp
@@ -27,3 +27,35 @@ public:
         return root;
     }
 };
+
+/////////////////////
+// Rewrite: no use of two vectors
+/////////////////////
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+    TreeNode *DFS(vector<int> &nums, int start, int end) {
+        if (start > end)
+            return NULL;
+        int mid = start + (end - start) / 2;
+        TreeNode *root = new TreeNode(nums[mid]);
+        root->left = DFS(nums, start, mid - 1);
+        root->right = DFS(nums, mid + 1, end);
+        return root;
+    }
+public:
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 0)
+            return NULL;
+        
+        return DFS(nums, 0, nums.size() - 1);
+    }
+};
