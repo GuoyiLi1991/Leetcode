d478b330dfd241dd5224dcf7d655e2879c803900
flyvbee <wxn.vicky@gmail.com>
2016-07-21 23:29:53 -0700

    Happy number

--- 202. HappyNumber.cpp
+++ 202. HappyNumber.cpp
@@ -1,9 +1,10 @@
 class Solution {
+    int sq[10] = {0, 1, 4, 9, 16, 25, 36, 49, 64, 81};
     int ssd(int n) {
         int sum = 0;
         while (n) {
             int d = n % 10;
-            sum += d * d;
+            sum += sq[d];
             n /= 10;
         }
         return sum;
