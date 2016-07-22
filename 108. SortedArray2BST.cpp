2418957123c4d78c04f3139dc17d06982aa2ea3e
flyvbee <wxn.vicky@gmail.com>
2016-07-21 22:09:28 -0700

    sorted array to BST

---
+++ 108. SortedArray2BST.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 0)
+            return NULL;
+        if (n == 1)
+            return new TreeNode(nums[0]);
+        
+        int mid = n/2;
+        TreeNode *root = new TreeNode(nums[mid]);
+    
+        vector<int> left(nums.begin(), nums.begin() + mid);
+        vector<int> right(nums.begin() + mid + 1, nums.end());
+        root->left = sortedArrayToBST(left);
+        root->right = sortedArrayToBST(right);
+        
+        return root;
+    }
+};
