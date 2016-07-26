0a0eefd1fe1cb1ca801021961ddc5da4fe31a361
flyvbee <wxn.vicky@gmail.com>
2016-07-25 22:29:44 -0700

    flatten BT

---
+++ 114. FlattenBinaryTree.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        TreeNode *cur = root;
+        while (cur) {
+            if (cur->left) {
+                //move cur->right to the rightchild of the rightmost leave of cur->left
+                TreeNode *ptr = cur->left;  
+                while (ptr->right)
+                    ptr = ptr->right;
+                    
+                ptr->right = cur->right;
+                
+                cur->right = cur->left;
+                cur->left = NULL;
+            }
+            cur = cur->right;  //move down current
+        }
+    }
+};
