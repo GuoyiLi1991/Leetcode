a8e9d8f76b9c4d548ce38b4a1c0877729eb438b0
flyvbee <wxn.vicky@gmail.com>
2016-07-26 16:14:21 -0700

    iterative invert BT

--- 226.InvertBinaryTree.cpp
+++ 226.InvertBinaryTree.cpp
@@ -10,11 +10,37 @@
 class Solution {
 public:
     TreeNode* invertTree(TreeNode* root) {
-        if (!root) return root;
+        if (!root) 
+        	return root;
+        if (!root->left && !root->right)
+        	return root;
         swap(root->left, root->right);
         invertTree(root->left);
         invertTree(root->right);
         return root;
         
+    }
+};
+
+//////////////////////////////////////
+// iterative
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if (!root)
+            return root;
+        queue<TreeNode *> st;
+        st.push(root);
+        
+        while (!st.empty()) {
+            TreeNode *tmp = st.front();
+            swap(tmp->left, tmp->right);
+            st.pop();
+            if (tmp->left)
+                st.push(tmp->left);
+            if (tmp->right)
+                st.push(tmp->right);
+        }
+        return root;
     }
 };
