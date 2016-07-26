dfee88a9a74206abf7102c56abddd3413e3b19c2
flyvbee <wxn.vicky@gmail.com>
2016-07-26 16:21:06 -0700

    update addDigits

--- 258.AddDigit.cpp
+++ 258.AddDigit.cpp
@@ -5,3 +5,21 @@ public:
         return (num-1)%9+1;
     }
 };
+//////////////////////
+// Basic version
+class Solution {
+    int getSum(int num) {
+        int sum = 0;
+        while (num) {
+            sum += num % 10;
+            num /= 10;
+        }
+        return sum;
+    }
+public:
+    int addDigits(int num) {
+        if (num / 10 == 0)
+            return num;
+        return addDigits(getSum(num));
+    }
+};
