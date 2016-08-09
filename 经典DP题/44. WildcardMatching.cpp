a5255e653f13bea1d72e74c3fa9267bdb2c14f07
flyvbee <wxn.vicky@gmail.com>
2016-08-08 18:30:25 -0700

    add other ppl's sol to wildcard matching

--- 经典DP题/44. WildcardMatching.cpp
+++ 经典DP题/44. WildcardMatching.cpp
@@ -20,9 +20,10 @@ public:
         //i > 0, j > 0
         for (int i = 1; i <= m; i++) 
             for (int j = 1; j <=  n; j++) 
-                if (p[j-1] == '?')
+                if (p[j-1] == '?')  //ignoring checking the cur char, totally depends on the history
                     f[i][j] = f[i-1][j-1];
-                else if (p[j-1] == '*')
+                else if (p[j-1] == '*')  
+                //true either 1)already matches s completely, but with trailing stars 2)"*" can deal with all the rest
                     f[i][j] = f[i][j-1] || f[i-1][j] ;
                 else
                     f[i][j] = f[i-1][j-1] && (s[i-1] == p[j-1]);
@@ -44,3 +45,73 @@ public:
     //     return isMatch(s, p.substr(1)) || !s.empty() && isMatch(s.substr(1), p);
         
     // }
+
+
+////////// Someone else's solutions
+// Analysis: https://discuss.leetcode.com/topic/21577/my-three-c-solutions-iterative-16ms-dp-180ms-modified-recursion-88ms/2
+////////// DP with trick
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int m = s.size(), n = p.size();
+        int cur, prev;
+        if (!n) return !m;
+        // vector<vector<bool>> matched(2, vector<bool>(m + 1, false)); // 1400ms
+        bool matched[2][m+1];  //192ms
+        fill_n(&matched[0][0], 2 * (m+1), false);
+        
+        matched[0][0] = true; //both are empty
+        for (int i = 1; i <= n; i++) {
+            cur = i % 2, prev = 1 - cur;
+            matched[cur][0] = matched[prev][0] && p[i-1] == '*';
+            if (p[i-1] == '*')
+                for (int j = 1; j <= m; j++)
+                    matched[cur][j] = matched[cur][j-1] || matched[prev][j];
+            else 
+                for (int j = 1; j <= m; j++)
+                    matched[cur][j] = matched[prev][j-1] && (p[i-1] == '?' || p[i-1] == s[j-1]);
+        }
+        
+        return matched[cur][m];
+    }
+};
+
+/////// DFS recursion: 88ms
+class Solution {
+private:
+    bool helper(const string &s, const string &p, int si, int pi, int &recLevel)
+    {
+        int m = s.size(), n = p.size();
+        int curLevel = recLevel;
+
+        bool first=true;
+        while(si < m && (p[pi]==s[si] || p[pi]=='?')) {++pi; ++si;} //match as many as possible
+        if(pi == n) return si == m; // if p reaches the end, return
+        
+        if(p[pi]=='*') { // if a star is met
+            while (p[++pi] == '*'); //skip all the following stars
+            if(pi >= n) return true; // if the rest of p are all star, return true
+            
+            for(int i = si; i<m; ++i)
+            {   // then do recursion
+                if(p[pi]!= '?' && p[pi]!=s[i]) continue;
+                if(first) {
+                    ++recLevel; 
+                    first = false;
+                }
+                
+                if(helper(s, p, i, pi, recLevel)) return true;
+                if(recLevel>curLevel+1) return false; // if the currently processed star is not the last one, return
+            }
+            
+        }
+        return false;
+    }
+public:
+    bool isMatch(string s, string p) {
+        int recLevel = 0;
+        return helper(s, p, 0, 0, recLevel);
+    }
+};
+
+
