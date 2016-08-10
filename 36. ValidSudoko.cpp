9aa62ad20637284a0ca98a6b6dcee8769af37bf9
flyvbee <wxn.vicky@gmail.com>
2016-08-09 19:10:51 -0700

    update valid sudoko

--- 36. ValidSudoko.cpp
+++ 36. ValidSudoko.cpp
@@ -22,3 +22,28 @@ public:
         return true;
     }
 };
+
+//////////// Another way to treat the box hash table
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        bool row[9][9] = {{0}}, col[9][9] = {{0}}, box[9][3][3] = {{{0}}}; //9 hash tables respectively
+        
+        for (int i = 0; i < 9; i++) {    //O(n^2)
+            for (int j = 0; j < 9; j++) {
+                char c = board[i][j];
+
+                if (c != '.') {
+                    int num = c - '1';
+                    if (num < 0 || num > 8)
+                        return false;
+                    
+                    if (row[i][num] || col[j][num] || box[num][i/3][j/3])
+                        return false;
+                    row[i][num] = col[j][num] = box[num][i/3][j/3] = 1;
+                }
+            }
+        }
+        return true;
+    }
+};
