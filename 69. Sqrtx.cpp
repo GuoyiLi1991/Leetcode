7ac5fbcf0ffd4805e328a66ab5fda1ff05f2c032
flyvbee <wxn.vicky@gmail.com>
2016-08-13 00:19:37 -0700

    update sqrtx

--- 69. Sqrtx.cpp
+++ 69. Sqrtx.cpp
@@ -17,3 +17,24 @@ public:
         return l;
     }
 };
+
+
+////////////
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x < 0) return -1;
+        if (x <= 1) return x;
+        int l = 0, r = x;
+        while (l <= r) {
+            int mid = l + (r - l)/2;
+            if (x/mid == mid)
+                return mid;
+            if (x / mid > mid)
+                l = mid + 1;
+            else
+                r = mid - 1;
+        }
+        return r;
+    }
+};
