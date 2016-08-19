c8d70bcc9ce8a9d6810c5ad3b639feebeb26302e
flyvbee <wxn.vicky@gmail.com>
2016-08-19 00:12:02 -0700

    comment on encode decode ways

--- 271. EncodeAndDecode.cpp
+++ 271. EncodeAndDecode.cpp
@@ -1,57 +1,48 @@
 class Codec {
-public:
-    //helper func: formatNum
-    string formatNum(int n)
-    {
+    string formatNum(int n) {
         stringstream ss;
-        ss <<setw(4)<<setfill('0')<<n;
+        ss << setw(4) << setfill('0') << n;
         return ss.str();
     }
+public:
+
     // Encodes a list of strings to a single string.
     string encode(vector<string>& strs) {
-        string s; //[num_strs, len1+len2+..., str1+str2...]
-        int num_strs = strs.size();
-        int idx = 0;
-        int idx_str = 4 + num_strs *4;
+        int n = strs.size();
+        string s; //s = "n, len0+len1+len2+...+lenn, str0+str1+str2+...+strn"
         
-        s += formatNum(num_strs);
+        //add n
+        s += formatNum(n);  //4bits
         
-        for (int i = 0; i<num_strs; i++)
-        {
-            s+=formatNum(strs[i].length());
-        }
-        for (int i = 0; i<num_strs; i++)
-        {
-            s+=strs[i];
-        }
-        
+        //add lengths
+        for (string word:strs)
+            s += formatNum(word.length()); //4*n bits
+            
+        //add strings
+        for (string word:strs) //sum(len(word)) bits
+            s+= word;
+            
         return s;
-        
     }
 
     // Decodes a single string to a list of strings.
     vector<string> decode(string s) {
-        vector<string> strs;
-        int idx = 0;
+        int n = stoi(s.substr(0, 4));
+        vector<string> res(n, "");
+        int i_len = 4, i_word = 4 + 4 * n;
         
-        int nums_strs = stoi(s.substr(idx,4));
-        idx = idx+4;
-        if (nums_strs == 0) return strs;
-        
-        int idx_str = nums_strs *4 +4; //idx of the first string
-        
-        for (int i = 0; i<nums_strs; i++)
-        {
-            int len = stoi(s.substr(idx, 4));
-            string tmp  = s.substr(idx_str, len);
-            strs.push_back(tmp);
-
-            idx += 4;
-            idx_str += len;
+        for (int i = 0; i < n; i++) {
+            // abstract res[i] = s.substr(i_word, len)
+            int len = stoi(s.substr(i_len, 4));
+            string word = s.substr(i_word, len);
+            res[i] = word;
+            
+            // pointers move forward
+            i_len += 4;
+            i_word += len;
         }
         
-        return strs;
-  
+        return res;
     }
 };
 
