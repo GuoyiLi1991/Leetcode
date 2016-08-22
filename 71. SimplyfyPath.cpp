cbe00be3f7798d9d7c0f667308f61126ed3f1c09
flyvbee <wxn.vicky@gmail.com>
2016-08-22 15:39:34 -0700

    simplify path

---
+++ 71. SimplyfyPath.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        int n = path.size();
+        string res;
+        stack<string> st;
+        for (int i = 0; i < n; i++){
+            string tmp;
+            while (i < n && path[i] != '/') {
+                tmp += path[i++];
+            }
+            if (tmp == "" || tmp == ".") continue; // multiple slashes or stays still
+            if (tmp == "..") {
+                if (!st.empty())
+                    st.pop(); //pop parent dir
+            }
+            else
+                st.push(tmp);
+        }
+        //build simplified path
+        while (!st.empty()) {
+            string tmp = st.top();
+            st.pop();
+            res = '/' + tmp + res;
+        }
+        return  (res == "")? "/": res;
+    }
+};
