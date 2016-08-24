cb990da354f8ab3baa941833d5f582356f5df180
flyvbee <wxn.vicky@gmail.com>
2016-08-24 00:17:24 -0700

    388. absolute path

---
+++ 388. LongestAbsolutePath.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int lengthLongestPath(string input) {
+        int n = input.size();
+        if (n == 0) return 0;
+        
+        stack<int> st;
+        int i = 0;
+        int res = 0;
+        string line;
+        while (i < n + 1) {
+            if (i != n && input[i] != '\n')
+                line += input[i];
+            else { //parse the line
+                int level = line.find_first_not_of('\t');
+                line = line.substr(level); //remove tabs
+                
+                while (level < st.size()) //find the correct level
+                    st.pop();
+                    
+                if (st.empty())     //first level
+                    st.push(line.size());
+                else {              // already under some folder
+                    st.push(line.size() + st.top() + 1);
+                }
+                
+                if (line.find('.') != string::npos && st.top() > res)
+                    res = st.top();
+                line = "";
+            }
+            i++;
+        }
+    
+        return res;
+    }
+};
