b6b3afaafc34846dd62a9e5f3aba494a58cf3246
flyvbee <wxn.vicky@gmail.com>
2016-09-12 13:06:37 -0700

    search2DMatrix

--- 74. Search2DMatrix.cpp
+++ 74. Search2DMatrix.cpp
@@ -64,5 +64,41 @@ public:
                 else if(matrix[row][mid]>target)  end=mid;
             }
             return matrix[row][start]==target;
+    }
+};
+
+///////////
+// Redo
+class Solution {
+    bool BS(vector<int>& v, int target) {
+        int l = 0, r = v.size() - 1;
+        if (v[l] > target || v[r] < target)
+                return false;
+        while (l + 1 < r) {
+            int mid = l + (r - l)/2;
+            if (v[mid] == target)
+                return true;
+            if (v[mid] < target)
+                l = mid;
+            else 
+                r = mid;
         }
-    };
+        if (v[l] == target || v[r] == target)
+            return true;
+        return false;
+    }
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        if (m == 0) return -1;
+        int n = matrix[0].size();
+        if (n == 0) return -1;
+        
+        for (int i = 0; i < m; i++)
+            if (BS(matrix[i], target))
+                return true;
+        
+        return false;
+     }
+};
+
