d517e9c33fe905f44e7397724c7fcd68309b4f68
flyvbee <wxn.vicky@gmail.com>
2016-10-06 18:04:01 -0700

    peak element followup

--- 162. FindPeakElement.cpp
+++ 162. FindPeakElement.cpp
@@ -57,3 +57,24 @@ public:
         return l;
     }
 };
+
+
+/////=======================================//
+// Follow up: 
+// Given a matrix(最外周比次外周小）, find peak element
+// A[i][j] > A[i + 1][j]
+// A[i][j] > A[i - 1][j]
+// A[i][j] > A[i][j + 1]
+// A[i][j] > A[i][j - 1]
+
+
+// Sol1 : 暴力搜索（O(4mn))
+// Sol2 : 选行／列 一个维度进行二分＝》如果只是局部最优解，可能会绕圈，不可行
+// sol3 : 找第m/2行“整行最大值”：纵向比较：O(nlogm)
+// if (a[i][j] < a[i + 1][j]) search in the upper half
+// if (a[i][j] < a[i - 1][j]) search in the lower half
+// if (a[i][j] > a[i + 1][j] && a[i][j] > a[i - 1][j]) return
+// 进一步优化：行列两个维度都二分＝》线性复杂度
+// T(nm) = m + n/2 + T(m/2 * n/2) 
+//        = m(1 + 1/2 + 1/4+....) + n(1/2+1/4+...) 
+//        = O(2m + n)
