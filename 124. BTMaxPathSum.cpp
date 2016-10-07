d4a7025ae5872cfc15d10bd758c328253f6c9dc2
flyvbee <wxn.vicky@gmail.com>
2016-10-06 23:31:22 -0700

    level order traversal better understanding

--- 124. BTMaxPathSum.cpp
+++ 124. BTMaxPathSum.cpp
@@ -2,7 +2,7 @@
 // Time: O(n^2)
 
 // Sol2. Divide & conquer
-// Time: O(n)
+// Time: O(n), Space: O(h), h is depth of recursion stack
 class ResultType{
 public:
     int singlePath; //at least 0
@@ -25,7 +25,7 @@ class Solution {
         ResultType* right = helper(root->right);
         
         //conquer
-        int sp = max(0, max(left->singlePath, right->singlePath) + root->val);
+        int sp = max(0, max(left->singlePath, right->singlePath) + root->val); //要么取root要么不取
         int mp = max(left->maxPath, max(right->maxPath, left->singlePath + right->singlePath + root->val));
         
         return new ResultType(sp, mp);
@@ -33,5 +33,27 @@ class Solution {
 public:
     int maxPathSum(TreeNode* root) {
         return helper(root)->maxPath;
+    }
+};
+
+// Sol 2-2: same idea, more concise by holding a global var maxValue
+class Solution {
+    int maxValue;
+    int maxPathDown(TreeNode* node) { // contain only downwards direction
+        if (!node) return 0;
+        
+        //divide
+        int left = max(0, maxPathDown(node->left));
+        int right = max(0, maxPathDown(node->right));
+        
+        //conquer
+        maxValue = max(maxValue, left + right + node->val);
+        return max(left, right) + node->val;
+    }
+public:
+    int maxPathSum(TreeNode* root) {
+        maxValue = INT_MIN;
+        maxPathDown(root);
+        return maxValue;
     }
 };
