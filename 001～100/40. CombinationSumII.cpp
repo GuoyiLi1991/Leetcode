17e4bb2308334c2dbfc3a12409289b180609b625
flyvbee <wxn.vicky@gmail.com>
2016-10-09 16:20:46 -0700

    combination sum II dealing with duplicated candidate

--- 001～100/40. CombinationSumII.cpp
+++ 001～100/40. CombinationSumII.cpp
@@ -1,36 +1,33 @@
-// contains duplication
+//相同元素只能最多取freq次
+// 1. dfs之前要sort
+// 2. dfs时 pos = i + 1
+// 3. bt之后while循环跳过重复元素
 class Solution {
-    void backtracking(vector<vector<int>> &result, vector<int> &list, vector<int>&candidates, int target, int level)
-    {
-        if (target == 0)
-        {
-            result.push_back(list);
+    void dfs(vector<vector<int>> & res, vector<int> & list, vector<int>&candidates, int target, int pos) {
+        //termination cond.
+        if (target <= 0) {
+            if (target == 0)
+                res.push_back(list);
             return;
         }
         
-        if (target < 0)
-            return;
-            
-        for (int i = level; i < candidates.size(); ++i)
-        {
-            int num = candidates[i];
-            target -= num;
-            list.push_back(num);
-            backtracking(result, list, candidates, target, i+1);
+        //else, dfs
+        for (int i = pos; i < candidates.size(); i++) {
+            list.push_back(candidates[i]);
+            dfs(res, list, candidates, target - candidates[i], i + 1);
             list.pop_back();
-            target += num;
-            //deal with duplication
-            while ( i< candidates.size() && candidates[i+1] == num) i++;
+            
+            while (i < candidates.size() - 1 && candidates[i + 1] == candidates[i])
+                i++;
         }
     }
 public:
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        vector<vector<int>> result;
+        vector<vector<int>> res;
         vector<int> list;
-        
+        //要sort!
         sort(candidates.begin(), candidates.end());
-        backtracking(result, list, candidates, target, 0);
-        
-        return result;
+        dfs(res, list, candidates, target, 0);
+        return res;
     }
 };
